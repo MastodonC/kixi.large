3af8f4afacdbcdecdfc6b281b223a779cf02f513
bld <bruce@mastodonc.com>
2021-11-30 17:11:38 +0000

    Bump version of POI
    And sort of give up on the charts every really being scaled properly

--- src/kixi/large.clj
+++ src/kixi/large.clj
@@ -21,7 +21,7 @@
 
 (defn add-image! [^Sheet sheet
                   {::keys [image col-anchor row-anchor]
-                   :or {col-anchor 10
+                   :or {col-anchor 15
                         row-anchor 1}
                    :as config}]
   (try
@@ -31,14 +31,15 @@
             helper (.getCreationHelper workbook)
             drawing (.createDrawingPatriarch sheet)
             anchor (.createClientAnchor helper)
-            col-width 13.0
-            row-height 36.0
             _ (.setCol1 anchor col-anchor)
             _ (.setRow1 anchor row-anchor)
             ;; Picture pict = drawing.createPicture(anchor, pictureIdx);
             ;; pict.resize();
             pict (.createPicture drawing anchor pic-idx)]
-        (.resize pict col-width row-height)))
+        #_(.resize pict Double/MAX_VALUE Double/MAX_VALUE) ;; doesn't properly size pic
+        #_(.resize pict 1.0 1.0) ;; doesn' properly size pic
+        (.resize pict) ;; needed or picture doesn't show up
+        ))
     (catch Exception e
       (throw (ex-info "Failed to add image" config e)))))
 
